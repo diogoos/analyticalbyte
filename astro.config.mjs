95e7423a4831c8efe611fbf7632b2dce118136b3
Diogo Silva <59505963+diogoos@users.noreply.github.com>
2025-08-14 10:30:48 -0300

    restyle & configure blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,10 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: "tokyo-night",
+			wrap: true
+		}
+	}
 });
---
+++ src/content/blog/about.md
@@ -0,0 +1,7 @@
+---
+title: 'About me'
+description: 'About'
+pubDate: 'Jan 01'
+_static: true
+---
+Hey there!
